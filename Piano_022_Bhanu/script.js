8e06a5b50c29b65eb46d1c0cbf9e52abfe7b5d3f
Vijayabhanuuu <72122923+Vijayabhanuuu@users.noreply.github.com>
2021-03-17 11:46:56 +0530

    paino

--- Piano_022_Bhanu/script.js
+++ Piano_022_Bhanu/script.js
@@ -1,25 +1,10 @@
-const WHITE_KEYS = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
-const BLACK_KEYS = ['h', 'i', 'j', 'k', 'l']
 
 const keys = document.querySelectorAll('.key')
-const whiteKeys = document.querySelectorAll('.key.white')
-const blackKeys = document.querySelectorAll('.key.black')
 
 keys.forEach(key => {
   key.addEventListener('click', () => playNote(key))
 })
 
-document.addEventListener('keydown', e => {
-  if (e.repeat) return 
-  const key = e.key
-  const whiteKeyIndex = WHITE_KEYS.indexOf(key)
-  const blackKeyIndex = BLACK_KEYS.indexOf(key)
-
-  if (whiteKeyIndex > -1) 
-  playNote(whiteKeys[whiteKeyIndex])
-  if (blackKeyIndex > -1) 
-  playNote(blackKeys[blackKeyIndex])
-})
 
 function playNote(key) {
   const noteAudio = document.getElementById(key.dataset.note)
